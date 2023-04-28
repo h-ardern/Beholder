4ffe2c3856781917a66d341ab8de6ca26de98c94
Henry Ardern <39902585+08ardernh@users.noreply.github.com>
2023-04-28 11:01:42 +0100

    Initial commit

---
+++ beholder.py
@@ -0,0 +1,59 @@
+from face_detect import face_rec
+from face_capture import add_person
+from sys import platform
+from trainer import train
+import os
+import cv2
+
+# Path for face image database
+path = 'dataset'
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+detector = cv2.CascadeClassifier("Cascades/haarcascade_frontalface_default.xml")
+
+
+def splash():
+    s = open('Splash.txt', 'r', encoding='utf-8')
+    splash_screen = s.read()
+    print(splash_screen)
+    s.close()
+
+
+def init():
+    option = int(input(
+        'BEHOLDER _ INPUT//: Please press 1 to add a person to the dataset \n                     Press 2 to run the system'))
+    if option == 1:
+        add_usr()
+    elif option == 2:
+        face_rec(camera_select())
+    else:
+        print('Not a valid input')
+        init()
+
+
+def add_usr():
+    add_person()
+    train(path, recognizer, detector)
+
+
+def camera_select():
+    cam = input(
+        " BEHOLDER _ INPUT//: PLEASE ENTER ID OF DESIRED CAMERA\n[PRESS ENTER FOR DEFAULT | ENTER \'L\' FOR LIST OF DEVICES} - ")
+    if cam == "":
+        cam = 0
+    elif cam == 'L' or cam == 'l':
+        if platform == "darwin":
+            os.system('ioreg | grep -i cam')
+            cam = camera_select()
+            return cam
+        elif platform == "win32" or platform == "win64":
+            pass
+    else:
+        try:
+            cam = int(cam)
+        except:
+            print("ERROR NOT A VALID DEVICE")
+    return cam
+
+
+splash()
+init()
---
+++ trainer.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+from PIL import Image
+import os
+
+# function to get the images and label data
+def getImagesAndLabels(path,detector):
+    imagePaths = [os.path.join(path,f) for f in os.listdir(path)]
+    faceSamples=[]
+    ids = []
+    for imagePath in imagePaths:
+        PIL_img = Image.open(imagePath).convert('L') # grayscale
+        img_numpy = np.array(PIL_img,'uint8')
+        ascii_id = ''.join(str(ord(c)) for c in os.path.split(imagePath)[-1].split(".")[1])
+        id = int(ascii_id)
+        faces = detector.detectMultiScale(img_numpy)
+        for (x,y,w,h) in faces:
+            faceSamples.append(img_numpy[y:y+h,x:x+w])
+            ids.append(id)
+    return faceSamples,ids
+
+def train(path,recognizer,detector):
+    print ("\n [INFO] Training faces. It will take a few seconds. Wait ...")
+    faces,ids = getImagesAndLabels(path, detector)
+    recognizer.train(faces, np.array(ids))
+    # Save the model into trainer/trainer.yml
+    recognizer.write('trainer/trainer.yml')
+    # Print the numer of faces trained and end program
+    print("\n [INFO] {0} faces trained. Exiting Program".format(len(np.unique(ids))))
